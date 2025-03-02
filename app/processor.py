cd53200e64fc29f611574ff4c71c6b33a5d19c66
mislavskii <jornjat@gmail.com>
2025-03-02 16:44:08 +0700

    made processor accept path

--- app/processor.py
+++ app/processor.py
@@ -11,12 +11,17 @@ def make_markdown(path: str):
     transcript_id = chunks[-1]
     video_id = chunks[-2]
     title = ' '.join(chunks[1:-2])
-    print(transcript_id, video_id, title, sep='\n')
     with open(f'files/{file_name}.md', 'w', encoding='utf-8') as f:
         f.write(f'# [{title}]({YT_BASE_URL}{video_id})\n\n')
         f.write(f'transcript_id: {transcript_id}\n\n')
         f.write(text)
 
-path = r'F:\User\Data\Endeavors\Speech2Text\files\transcript_Rabbi Sends STRONG Message To The World From Jerusalem_X7Iav-Cr2C8_f5bbfeb7-fd78-4999-a947-1edfb3f17ff5.txt'
 
-make_markdown(path)
+def main():
+    path = input('Enter the path to the transcript file: ')
+    make_markdown(path)
+    print('Markdown file created.')
+
+
+if __name__ == '__main__':
+    main()
